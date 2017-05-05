bfb5a589ff80bfbb2a130ec88e45730d4c25fe33
RoCky Wu <rocky823@gmail.com>
2017-05-06 09:59:16 +1000

    Add deploy to gh-pages

---
+++ scripts/deploy.js
@@ -0,0 +1,30 @@
+const { version, repository, author } = require('../package.json')
+require('shelljs/global')
+const { TRAVIS_BRANCH, TRAVIS_MATRIX, TRAVIS_PULL_REQUEST_BRANCH,
+  GH_TOKEN, NPM_TOKEN, SLACK_TOKEN } = process.env
+
+const tokenRepo = repository.replace(/(github.com)/, `${GH_TOKEN}@$1`)
+const tag = `v${version}`
+
+console.log({ TRAVIS_BRANCH, TRAVIS_MATRIX, TRAVIS_PULL_REQUEST_BRANCH })
+
+
+if (TRAVIS_BRANCH === 'master') {
+  const targetMatrix = {
+    'build'() {
+      exec(`git config --global user.email "auto_deploy@travis-ci.org"`)
+      exec(`git config --global user.name "TravisCI"`)
+
+      // Publish to gh-pages
+      cd('gh-pages')
+      exec('git init')
+      exec('git add .')
+      exec(`git commit -anm '${version}'`)
+      exec(`git push ${tokenRepo} master:gh-pages -f`)
+    }
+  }[TRAVIS_MATRIX]
+
+  if (targetMatrix) {
+    targetMatrix()
+  }
+}
