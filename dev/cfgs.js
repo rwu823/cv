d54f099c4810c90a61a127d75caefce6e464ab9c
RoCky Wu <rocky823@gmail.com>
2017-10-09 13:15:13 +0800

    New simple code base: - remove webpack - Fix CI deploy to `gh-pages` - One static HTML

---
+++ dev/cfgs.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+module.exports = {
+  port: 3030,
+  ghPrefix: '/cv',
+  staticDir: path.join(__dirname, '../src')
+};
--- scripts/publish.js
+++
@@ -1,8 +0,0 @@
-require('shelljs/global')
-const { version, repository } = require('../package.json')
-
-cd('gh-pages')
-exec('git init')
-exec('git add .')
-exec(`git commit -anm "v${version}"`)
-exec(`git push ${repository} master:gh-pages -f`)
