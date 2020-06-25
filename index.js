c19bdb21b149363e64b2aa337f6925fc95e56255
rocky <rocky@17.media>
2020-06-25 15:01:02 +0800

    update

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const CFonts = require('cfonts')
   })
 
   console.log(`
-${c.cyan.dim('Coding, Software & Live with JavaScript')}
+${c.cyan('Coding, Software & Live with JavaScript')}
 
 Hello I am Rocky, thank you for taking the time to read my experience. And you can get more about me through the links below:
 
